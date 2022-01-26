c6d625ba6e812c17f31f8b5fe618615d8c18bd92
praveenkl3043 <lalpraveenkl@gmail.com>
2022-01-26 23:53:54 +0530

    api2

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const blush = document.getElementById("blush")
-const result =document.getElementById("result")
+const btn=document.getElementsByClassName("btn btn-primary");
+const result=document.getElementsByClassName("data");
 
-const url="http://makeup-api.herokuapp.com/api/v1/products.json";
+const url="https://api.github.com/users/praveenkl3043";
 
-almay.addEventListener("click",async function() {
-    try{
-        let res  = await fetch(url);
-        let data= await res.json();
-       result.innerHTML=`
-       ${data[0].name}
-       `
-       
-           
-       }
-       catch (error){
-        console.log(error);
-    }
-})
+btn[0].addEventListener("click",async function (){
 
 
+    result[0].innerHTML="loading...";
+     try{
+         let res= await fetch(url);
+         let objet=await res.json();
+         console.log(objet)
+         result[0].innerHTML=`
+         <h6 class="login">Login:<span>${objet.login}<span></h6>
+         <h6 class="id">id:<span>${objet.id}</span></h6>
+         <h6 class="repo">public repos:<span>${objet.public_repos}</span></h6>
+         <h6 class="githubprofile">Githubprofile:<span>${objet.html_url}</span></h6>
+         
+         `
+     }catch (error){
+         console.log(error);
+         
+     }
+ });
